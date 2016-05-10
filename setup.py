6a6951e855a8797c691ccb6414b3bb73ab5b9e4c
Tommy Wang <tommy.wang@rewardstyle.com>
2016-05-10 14:05:51 -0500

    now with config file handling

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     description='utility class for handling argparse subparsers',
     py_modules=['subparser'],
+    install_requires=['six']
     author='Tommy Wang',
     author_email='twang@august8.net',
     license='PSF',
--- subparser.py
+++ subparser.py
@@ -1,29 +1,199 @@
-#!/usr/bin/env python
 import argparse
+import collections
+import contextlib
+import functools
+import sys
+
+from six.moves import configparser
 
 
-class subcommand(object):
-    _parser = argparse.ArgumentParser()
-    _subparser = _parser.add_subparsers(dest='command')
-    def __new__(cls, command_or_f=None, command=None):
-        if isinstance(command_or_f, basestring):
-            return lambda f: subcommand(f, command_or_f)
-        elif callable(command_or_f):
-            return object.__new__(cls)
-    def __init__(self, function, command=None):
-        self.parser = self._subparser.add_parser(command or function.__name__)
-        self.parser.set_defaults(function=function)
+class DispatchWrapper(object):
+    '''
+    wraps a dispatch function and creates a subparser out of it
+    '''
+    def __init__(self, subparser, func, name=None):
+        self.func = func
+        self.name = name or func.__name__
+        self.parser = subparser.add_parser(self.name)
+        self.parser.set_defaults(func=self.func)
+        functools.update_wrapper(self, self.func)
+
     def __call__(self, *args, **kwargs):
-        return self.function(*args, **kwargs)
-    def __getattr__(self, key):
-        return getattr(self.parser, key)
-    @classmethod
-    def parse_args(cls, *args, **kwargs):
-        return cls._parser.parse_args(*args, **kwargs)
-    @classmethod
-    def dispatch(cls, *args, **kwargs):
-        ns = cls._parser.parse_args(*args, **kwargs)
-        return ns.function(ns)
-    @classmethod
-    def set_defaults(cls, *args, **kwargs):
-        cls._parser.set_defaults(*args, **kwargs)
+        '''
+        allows wrapped function to still be called
+        '''
+        return self.func(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        '''
+        provides access to ArgumentParser functions
+        '''
+        return getattr(self.parser, attr)
+
+
+class Subcommand(object):
+    '''
+    multi-use object:
+        - can be called to be used as a decorator to wrap dispatch functions
+        - dispatch allows us to process the command-line and run the dispatch function
+        - add_config adds an option to loads a config file prior to handling command-line options
+        - all other methods are passed to the main parser
+    '''
+    def __init__(self, parser, config):
+        self.parser = parser
+        parser_factory = functools.partial(type(parser), config)
+        self.subparser = parser.add_subparsers(dest='command',
+                                               parser_class=parser_factory)
+        self.config_parser = None
+        self.config_action = None
+        self.config = config
+
+    def __call__(self, name_or_func=None):
+        '''
+        decorator to wrap dispatch functions
+        '''
+        if isinstance(name_or_func, basestring):
+            def _decorator(f):
+                return DispatchWrapper(self.subparser, f, name_or_func)
+            return _decorator
+        elif callable(name_or_func):
+            return DispatchWrapper(self.subparser, name_or_func)
+        raise Exception('unrecognized argument to Subcommand')
+
+    def __getattr__(self, attr):
+        '''
+        act as a ArgumentParser for the main parser
+        '''
+        return getattr(self.parser, attr)
+
+    def add_config(self, *args, **kwargs):
+        '''
+        add a config option to load config files prior to command-line
+        '''
+        self.config_parser = argparse.ArgumentParser(add_help=False)
+        self.config_action = self.config_parser.add_argument(*args, **kwargs)
+        self.parser._add_container_actions(self.config_parser)
+
+    def dispatch(self, args=None, namespace=None):
+        '''
+        process args and dispatch appropriate dispatch function
+        '''
+        if self.config_parser:
+            namespace, args = self.config_parser.parse_known_args(args, namespace)
+            self.config.impl = getattr(namespace, self.config_action.dest, None).config
+        ns = self.parser.parse_args(args, namespace)
+        return ns.func(ns)
+
+
+class ConfigFacade(object):
+    '''
+    an object that allows ConfigEnabledArgumentParser to have no knowledge of
+    how to actually get a config setting.  defaults to a pass-through if no
+    implementation is configured.
+    '''
+    def __init__(self, impl=None):
+        self.impl = impl
+
+    def get_default(self, setting):
+        if self.impl is None:
+            return setting
+        return self.impl.get_default(setting)
+
+
+class ConfigEnabledArgumentParser(argparse.ArgumentParser):
+    '''
+    an argparse.ArgumentParser that resolves default values of type _ConfigSetting
+    prior to handling command-line options.
+    '''
+    def __init__(self, config, *args, **kwargs):
+        self.config = config
+        super(ConfigEnabledArgumentParser, self).__init__(*args, **kwargs)
+
+    def parse_known_args(self, args=None, namespace=None):
+        # default Namespace built from parser defaults
+        if namespace is None:
+            namespace = argparse.Namespace()
+
+        # add any action defaults that aren't present
+        for action in self._actions:
+            if action.dest is not argparse.SUPPRESS:
+                if not hasattr(namespace, action.dest):
+                    if action.default is not argparse.SUPPRESS:
+                        setattr(namespace, action.dest, self.config.get_default(action.default))
+
+        # add any parser defaults that aren't present
+        for dest in self._defaults:
+            if not hasattr(namespace, dest):
+                setattr(namespace, dest, self.config.get_default(self._defaults[dest]))
+
+        # call parse_known_args on parent
+        return super(ConfigEnabledArgumentParser, self).parse_known_args(args, namespace)
+
+
+class _ConfigAction(argparse.Action):
+    '''
+    base action class for handling the config option.
+
+    creates a named tuple in namespace containing both the option value (source)
+    and the config.
+    '''
+    ConfigTuple = collections.namedtuple('Config', 'source config')
+
+    def __init__(self, *args, **kwargs):
+        default = kwargs.pop('default', None)
+        config = self.make_config()
+        config.load(default)
+        super(_ConfigAction, self).__init__(*args, default=self.ConfigTuple(default, config), **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        config = self.make_config()
+        config.load(values)
+        setattr(namespace, self.dest, self.ConfigTuple(values, config))
+
+    def make_config(self):
+        raise NotImplementedError
+
+
+class RawConfigParserAction(_ConfigAction):
+    '''
+    an action for handling config options that are parsed via RawConfigParser.
+    '''
+    Setting = collections.namedtuple('Setting', 'section option default')
+
+    def make_config(outer_self):
+
+        class ConfigImpl(configparser.RawConfigParser):
+            def get_default(self, setting):
+                if not isinstance(setting, outer_self.Setting):
+                    return setting
+                try:
+                    return self.get(setting.section, setting.option)
+                except (configparser.NoOptionError, configparser.NoSectionError):
+                    return setting.default
+
+            def load(self, source):
+                '''
+                loads the config given a source.
+                '''
+                if source is not None:
+                    with self.fetch(source) as fp:
+                        self.readfp(fp)
+
+            @contextlib.contextmanager
+            def fetch(self, source):
+                '''
+                fetches a config given a source.
+                '''
+                fp = open(source, 'r')
+                try:
+                    yield fp
+                finally:
+                    fp.close()
+
+        return ConfigImpl()
+
+
+def subparser(*args, **kwargs):
+    _config = ConfigFacade()
+    _parser = ConfigEnabledArgumentParser(_config, *args, **kwargs)
+    return Subcommand(_parser, _config)
