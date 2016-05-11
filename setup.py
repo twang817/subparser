49b09ad93680e8909e09ec31d18a18f2cb60aafc
Tommy Wang <tommy.wang@rewardstyle.com>
2016-05-11 11:05:26 -0500

    improve packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import io
+import os
+
+from setuptools import setup, find_packages
+
+version = io.open('subparser/_version.py').readlines()[-1].split()[-1].strip('"\'')
 
 setup(
     name='subparser',
-    version='0.0.2',
+    version=version,
+
     description='utility class for handling argparse subparsers',
-    py_modules=['subparser'],
-    install_requires=['six'],
+
     author='Tommy Wang',
     author_email='twang@august8.net',
+    url='http://github.com/twang817/subparser',
+    download_url='http://github.com/twang817/subparser/tarball/{version}'.format(version=version),
+
+    packages=find_packages(),
+    install_requires=['six'],
+
     license='PSF',
 )
